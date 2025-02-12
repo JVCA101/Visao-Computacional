9c1e043f3274d5461c1626b4d078d312ac2441c2
JVCA101 <joaovictordealmeid@gmail.com>
2025-02-12 09:26:09 -0300

    vgg13 and minor change in alexnet

---
+++ models/AlexNet.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+import numpy as np
+
+def AlexNet(input_shape, num_classes):
+    alex = Sequential([
+        Conv2D(96, 11, strides=(4,4), activation='relu', input_shape=input_shape),
+        MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+
+        Conv2D(256, 5, padding='same', activation='relu'),
+        MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+        
+        Conv2D(384, 3, padding='same', activation='relu'),
+        Conv2D(384, 3, padding='same', activation='relu'),
+        Conv2D(256, 3, padding='same', activation='relu'),
+        MaxPooling2D(pool_size=(3,3), strides=(2,2)),
+        
+        Flatten(),
+        Dense(9216, activation='relu'),
+        Dense(4096, activation='relu'),
+        Dense(4096, activation='relu'),
+        Dense(num_classes, activation='softmax')
+    ], name='AlexNet')
+    return alex
---
+++ models/VGG13.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+def VGG13(input_shape, num_classes):
+    vgg13 = Sequential([
+        Conv2D(filters=64, kernel_size=3, padding='same', strides=1, activation='relu', input_shape=input_shape),
+        Conv2D(filters=64, kernel_size=3, padding='same', strides=1, activation='relu'),
+        MaxPooling2D(pool_size=2, strides=2),
+        
+        Conv2D(filters=128, kernel_size=3, padding='same', strides=1, activation='relu'),
+        Conv2D(filters=128, kernel_size=3, padding='same', strides=1, activation='relu'),
+        MaxPooling2D(pool_size=2, strides=2),
+        
+        Conv2D(filters=256, kernel_size=3, padding='same', strides=1, activation='relu'),
+        Conv2D(filters=256, kernel_size=3, padding='same', strides=1, activation='relu'),
+        MaxPooling2D(pool_size=2, strides=2),
+
+        Conv2D(filters=512, kernel_size=3, padding='same', strides=1, activation='relu'),
+        Conv2D(filters=512, kernel_size=3, padding='same', strides=1, activation='relu'),
+        MaxPooling2D(pool_size=2, strides=2),
+
+        Flatten(),
+        Dense(4096, activation='relu'),
+        Dense(4096, activation='relu'),
+        Dense(1000, activation='relu'),
+        Dense(num_classes, activation='softmax')
+    ], name='VGG13')
+    return vgg13

7c14b23c046c9827cc941b1d51fcc826af876888
João Victor Corrêa de Almeida <128003420+JVCA101@users.noreply.github.com>
2025-02-12 17:31:52 +0000

    alex and vgg fixed

--- models/VGG13.py
+++ models/VGG13.py
@@ -6,18 +6,26 @@ def VGG13(input_shape, num_classes):
     vgg13 = Sequential([
         Conv2D(filters=64, kernel_size=3, padding='same', strides=1, activation='relu', input_shape=input_shape),
         Conv2D(filters=64, kernel_size=3, padding='same', strides=1, activation='relu'),
-        MaxPooling2D(pool_size=2, strides=2),
-        
-        Conv2D(filters=128, kernel_size=3, padding='same', strides=1, activation='relu'),
-        Conv2D(filters=128, kernel_size=3, padding='same', strides=1, activation='relu'),
-        MaxPooling2D(pool_size=2, strides=2),
-        
-        Conv2D(filters=256, kernel_size=3, padding='same', strides=1, activation='relu'),
-        Conv2D(filters=256, kernel_size=3, padding='same', strides=1, activation='relu'),
-        MaxPooling2D(pool_size=2, strides=2),
 
+        MaxPooling2D(pool_size=2, strides=2),
+        Conv2D(filters=128, kernel_size=3, padding='same', strides=1, activation='relu'),
+        Conv2D(filters=128, kernel_size=3, padding='same', strides=1, activation='relu'),
+
+        MaxPooling2D(pool_size=2, strides=2),
+        Conv2D(filters=256, kernel_size=3, padding='same', strides=1, activation='relu'),
+        Conv2D(filters=256, kernel_size=3, padding='same', strides=1, activation='relu'),
+        Conv2D(filters=256, kernel_size=3, padding='same', strides=1, activation='relu'),
+
+        MaxPooling2D(pool_size=2, strides=2),
         Conv2D(filters=512, kernel_size=3, padding='same', strides=1, activation='relu'),
         Conv2D(filters=512, kernel_size=3, padding='same', strides=1, activation='relu'),
+        Conv2D(filters=512, kernel_size=3, padding='same', strides=1, activation='relu'),
+
+        MaxPooling2D(pool_size=2, strides=2),
+        Conv2D(filters=512, kernel_size=3, padding='same', strides=1, activation='relu'),
+        Conv2D(filters=512, kernel_size=3, padding='same', strides=1, activation='relu'),
+        Conv2D(filters=512, kernel_size=3, padding='same', strides=1, activation='relu'),
+
         MaxPooling2D(pool_size=2, strides=2),
 
         Flatten(),

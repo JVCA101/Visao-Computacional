a1feb76347df6250b5b89b1b26b1fd2123fd00cc
JVCA101 <joaovictordealmeid@gmail.com>
2025-02-19 19:32:25 -0300

    little changes on alex and main

--- models/AlexNet.py
+++ models/AlexNet.py
@@ -3,15 +3,15 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropou
 
 def AlexNet(input_shape, num_classes):
     alex = Sequential([
-        Conv2D(96, 11, strides=(4,4), activation='relu', input_shape=input_shape),
+        Conv2D(filters=96, kernel_size=11, strides=(4,4), activation='relu', input_shape=input_shape),
 
         MaxPooling2D(pool_size=(3,3), strides=(2,2)),
-        Conv2D(256, 5, padding='same', activation='relu'),
+        Conv2D(filters=256, kernel_size=5, padding='same', activation='relu'),
 
         MaxPooling2D(pool_size=(3,3), strides=(2,2)),
-        Conv2D(384, 3, padding='same', activation='relu'),
-        Conv2D(384, 3, padding='same', activation='relu'),
-        Conv2D(256, 3, padding='same', activation='relu'),
+        Conv2D(filters=384, kernel_size=3, padding='same', activation='relu'),
+        Conv2D(filters=384, kernel_size=3, padding='same', activation='relu'),
+        Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'),
 
         MaxPooling2D(pool_size=(3,3), strides=(2,2)),
         

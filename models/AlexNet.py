281560a1bf43b04a62b78b0251d8a01708698173
JVCA101 <joaovictordealmeid@gmail.com>
2025-03-03 10:59:29 -0300

    removing old tf code

--- models/AlexNet.py
+++ models/AlexNet.py
@@ -27,7 +27,7 @@ class AlexNet(Module):
     self.flatten = Flatten()
     
     self.full_layer = Sequential(
-      Linear(9216, 4096),
+      Linear(6*6*256, 4096),
       ReLU(),
       Linear(4096, 4096),
       ReLU(),
@@ -40,28 +40,3 @@ class AlexNet(Module):
     out = self.flatten(out)
     out = self.full_layer(out)
     return out
-  
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
-
-# def AlexNet(input_shape, num_classes):
-#     alex = Sequential([
-#         Conv2D(filters=96, kernel_size=11, strides=(4,4), activation='relu', input_shape=input_shape),
-
-#         MaxPooling2D(pool_size=(3,3), strides=(2,2), padding='same'),
-#         Conv2D(filters=256, kernel_size=5, padding='same', activation='relu'),
-
-#         MaxPooling2D(pool_size=(3,3), strides=(2,2)),
-#         Conv2D(filters=384, kernel_size=3, padding='same', activation='relu'),
-#         Conv2D(filters=384, kernel_size=3, padding='same', activation='relu'),
-#         Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'),
-
-#         MaxPooling2D(pool_size=(3,3), strides=(2,2)),
-        
-#         Flatten(),
-#         Dense(4096, activation='relu'),
-#         Dense(4096, activation='relu'),
-#         Dense(num_classes, activation='softmax')
-
-#     ], name='AlexNet')
-#     return alex

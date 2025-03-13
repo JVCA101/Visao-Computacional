c934eaef1a02f2a5569c0ec348004ce18a34d97d
JVCA101 <joaovictordealmeid@gmail.com>
2025-03-13 13:45:13 -0300

    batchnorm and dropout on slexnet

--- models/AlexNet.py
+++ models/AlexNet.py
@@ -7,10 +7,12 @@ class AlexNet(Module):
 
       Conv2d(3, 96, kernel_size=11, stride=4),
       ReLU(),
+      BatchNorm2d(96),
       MaxPool2d(kernel_size=3, stride=2),
       
       Conv2d(96, 256, kernel_size=5, padding=2),
       ReLU(),
+      BatchNorm2d(256),
       MaxPool2d(kernel_size=3, stride=2),
       
       Conv2d(256, 384, kernel_size=3, padding=1),
@@ -20,14 +22,17 @@ class AlexNet(Module):
       ReLU(),
       Conv2d(384, 256, kernel_size=3, padding=1),
       ReLU(),
+      BatchNorm2d(256),
       MaxPool2d(kernel_size=3, stride=2)
     )
     
     self.flatten = Flatten()
     
     self.full_layer = Sequential(
+      Dropout(),
       Linear(6*6*256, 4096),
       ReLU(),
+      Dropout(),
       Linear(4096, 4096),
       ReLU(),
       Linear(4096, 10)
